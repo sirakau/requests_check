1e9634305d86a1e9af6c69bd2ec4a895b1560dc3
sirakau <34806347+sirakau@users.noreply.github.com>
2025-09-08 14:22:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def inspect_request():
     return jsonify(info), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
